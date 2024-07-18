5436980c84b139054ab36f2c259816c96ddc389e
diana.petre <diana.petre@vitals.co>
2024-07-18 10:59:48 +0300

    feat: wip create dashboard for users

---
+++ app/user-dashboard/page.tsx
@@ -0,0 +1,93 @@
+'use client';
+
+import React, { ChangeEvent, useMemo, useState } from 'react';
+import { User } from '../../data/user/types';
+import { mockedUsers } from '../../data/user/mockData';
+import {
+	Button,
+	IconButton,
+	Paper,
+	Stack,
+	Table,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableHead,
+	TableRow,
+	TextField,
+	Typography,
+} from '@mui/material';
+import { format } from 'date-fns';
+import { Delete as DeleteIcon, Edit as EditIcon } from '@mui/icons-material';
+import { NavigationAndUserInfo } from '../../components/navigation-and-user-info';
+
+export default function Dashboard() {
+	const [users, setUsers] = useState<User[]>(mockedUsers);
+	const [searchInput, setSearchInput] = useState('');
+
+	const handleDelete = (id: number): void => {
+		const updated = users.filter((user) => user.id !== id);
+		setUsers(updated);
+	};
+
+	const handleSearchChange = (event: ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+		setSearchInput(event.target.value);
+	};
+
+	const filteredUsers = useMemo(() => {
+		return users.filter((user) => user.firstName.toLowerCase().includes(searchInput.toLowerCase()));
+	}, [searchInput, users]);
+
+	return (
+		<NavigationAndUserInfo>
+			<Stack spacing={2} alignItems="end">
+				<Paper sx={{ width: '100%', overflow: 'hidden' }}>
+					<TextField
+						fullWidth
+						label="Search by User name"
+						variant="outlined"
+						value={searchInput}
+						onChange={handleSearchChange}
+						sx={{ m: 2, width: '25%' }}
+					/>
+					<TableContainer component={Paper}>
+						<Table sx={{ minWidth: 650 }} aria-label="projects table">
+							<TableHead sx={{ '& .MuiTableCell-head': { fontWeight: 'bold' } }}>
+								<TableRow>
+									<TableCell align="center">Name</TableCell>
+									<TableCell align="center">Created At</TableCell>
+									<TableCell align="center">Processed papers</TableCell>
+									<TableCell align="center">Actions</TableCell>
+								</TableRow>
+							</TableHead>
+							<TableBody>
+								{filteredUsers.map((user: User) => (
+									<TableRow hover key={user.id}>
+										<TableCell component="th" scope="row">
+											<Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+												{user.firstName} {user.lastName}
+											</Typography>
+											<Typography variant="body1">{user.email}</Typography>
+										</TableCell>
+										<TableCell align="center">{format(new Date(), 'dd/MM/yyyy')}</TableCell>
+										<TableCell align="center" component="th" scope="row">
+											10
+										</TableCell>
+										<TableCell align="center">
+											<IconButton aria-label="edit">
+												<EditIcon />
+											</IconButton>
+											<IconButton aria-label="delete" onClick={() => handleDelete(user.id)}>
+												<DeleteIcon />
+											</IconButton>
+										</TableCell>
+									</TableRow>
+								))}
+							</TableBody>
+						</Table>
+					</TableContainer>
+				</Paper>
+			</Stack>
+		</NavigationAndUserInfo>
+	);
+}
---
+++ data/user/mockData.ts
@@ -0,0 +1,53 @@
+import { User } from './types';
+
+export const mockedUsers: User[] = [
+	{
+		id: 1,
+		email: 'john.doe@example.com',
+		firstName: 'John',
+		lastName: 'Doe',
+		picture: 'https://example.com/images/john.jpg',
+	},
+	{
+		id: 2,
+		email: 'jane.smith@example.com',
+		firstName: 'Jane',
+		lastName: 'Smith',
+		picture: 'https://example.com/images/jane.jpg',
+	},
+	{
+		id: 3,
+		email: 'michael.johnson@example.com',
+		firstName: 'Michael',
+		lastName: 'Johnson',
+		picture: 'https://example.com/images/michael.jpg',
+	},
+	{
+		id: 4,
+		email: 'emily.brown@example.com',
+		firstName: 'Emily',
+		lastName: 'Brown',
+		picture: 'https://example.com/images/emily.jpg',
+	},
+	{
+		id: 5,
+		email: 'david.lee@example.com',
+		firstName: 'David',
+		lastName: 'Lee',
+		picture: 'https://example.com/images/david.jpg',
+	},
+	{
+		id: 6,
+		email: 'sophia.clark@example.com',
+		firstName: 'Sophia',
+		lastName: 'Clark',
+		picture: 'https://example.com/images/sophia.jpg',
+	},
+	{
+		id: 7,
+		email: 'robert.white@example.com',
+		firstName: 'Robert',
+		lastName: 'White',
+		picture: 'https://example.com/images/robert.jpg',
+	},
+];
