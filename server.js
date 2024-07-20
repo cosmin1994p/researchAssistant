3c41bd630939cc599a5410dd70bba380562c5a52
cosmin1994p <proscanu.cosmin@gmail.com>
2024-07-20 13:11:32 +0300

    add certificate for https connection(necessary for kinde auth)
    ;

---
+++ server.js
@@ -0,0 +1,26 @@
+const https = require('https');
+const { parse } = require('url');
+const next = require('next');
+const fs = require('fs');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const httpsOptions = {
+  key: fs.readFileSync('/home/cosmin/researchAssistant/research-assistant-UI/selfsigned.key'),
+  cert: fs.readFileSync('/etc/ssl/certs/nginx-selfsigned.crt')
+                         
+};
+
+const port = process.env.PORT || 3000;
+
+app.prepare().then(() => {
+  https.createServer(httpsOptions, (req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on https://localhost:${port}`);
+  });
+});
