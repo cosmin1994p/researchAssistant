e986f80ac9552a72e9e7e95125b2353b74d440cd
cosmin1994p <cosmin.proscanu@m-ware.eu>
2024-08-03 10:57:51 +0000

    server modifs

--- server.js
+++ server.js
@@ -10,30 +10,18 @@ const handle = app.getRequestHandler();
 const httpsOptions = {
   key: fs.readFileSync('/home/cosmin/researchAssistant/research-assistant-UI/selfsigned.key'),
   cert: fs.readFileSync('/etc/ssl/certs/nginx-selfsigned.crt')
-                         
+
 };
 
-<<<<<<< HEAD
 const port = 3000;
 const hostname = '192.168.4.120';
-=======
-const port = process.env.PORT || 3000;
->>>>>>> cdc9ada7086fa438d65b6d1f49abe33177e53eea
 
 app.prepare().then(() => {
   https.createServer(httpsOptions, (req, res) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
-<<<<<<< HEAD
   }).listen(port, hostname, (err) => {
     if (err) throw err;
-    console.log(`> Ready on https:${port}`);
+    console.log(`> Ready on https:${hostname}  ${port}`);
   });
-});
-=======
-  }).listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on https://localhost:${port}`);
-  });
-});
->>>>>>> cdc9ada7086fa438d65b6d1f49abe33177e53eea
+}); 
