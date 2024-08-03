c275053fd8c60d71d31819e14fe23b6cdff59d0b
cosmin1994p <cosmin.proscanu@m-ware.eu>
2024-08-03 09:54:55 +0000

    configurations: pm2files

---
+++ server.js
@@ -0,0 +1,27 @@
+const https = require('https');
+const { parse } = require('url');
+const next = require('next');
+const fs = require('fs');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const httpsOptions = {
+  key: fs.readFileSync('/home/cosmin/researchAssistant/research-assistant-UI/selfsigned.key'),
+  cert: fs.readFileSync('/etc/ssl/certs/nginx-selfsigned.crt')
+                         
+};
+
+const port = 3000;
+const hostname = '192.168.4.120';
+
+app.prepare().then(() => {
+  https.createServer(httpsOptions, (req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(port, hostname, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on https:${port}`);
+  });
+});
