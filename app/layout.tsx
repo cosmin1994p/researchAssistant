9826366c83be56dbad1135ee81915d76e8cdc84b
diana.petre <diana.petre@vitals.co>
2024-08-12 11:02:32 +0300

    feat: add into local db the user if it does not exist

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,15 +2,46 @@ import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import Authenticate from '../components/authenticate';
 import { NavigationAndUserInfo } from '../components/navigation-and-user-info';
 import React from 'react';
+import prisma from '../lib/db';
 
 export const metadata = {
 	title: 'Research Assistant',
 	description: 'Research Assistant, your research best buddy',
 };
 
+interface KindeUserMapping {
+	email: string;
+	name: string;
+	lastName: string;
+}
+
+async function ensureUserIsPresentInOurDb(kindeUser: KindeUserMapping): Promise<void> {
+	await prisma.user.upsert({
+		where: {
+			email: kindeUser.email,
+		},
+		create: {
+			email: kindeUser.email,
+			userName: kindeUser.name,
+			lastName: kindeUser.lastName,
+		},
+		update: {},
+	});
+}
+
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
 	const { isAuthenticated, getUser } = getKindeServerSession();
 	const isLogged = await isAuthenticated();
+	const user = await getUser();
+
+	if (user) {
+		// TODO: see if those fields can ever be null
+		await ensureUserIsPresentInOurDb({
+			email: user.email ?? 'default-email@bla.com',
+			lastName: user.family_name ?? 'Empty Family Name',
+			name: user.given_name ?? 'Empty Given Name',
+		});
+	}
 
 	return (
 		<html lang="en">
