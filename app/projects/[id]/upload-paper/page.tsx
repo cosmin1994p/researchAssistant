66562bf60c72975bbe3bcb9dab9d1ebc9ba845a1
diana.petre <diana.petre@vitals.co>
2024-08-12 10:38:48 +0300

    feat: protected routes for user authentication

--- app/projects/[id]/upload-paper/page.tsx
+++ app/projects/[id]/upload-paper/page.tsx
@@ -1,75 +1,23 @@
-'use client';
+import React from 'react';
+import { UploadPaperClientComponent } from '../../../../components/upload-paper';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { redirect } from 'next/navigation';
 
-import React, { useState } from 'react';
-import { Box, Button, Typography, List, ListItem, Stack } from '@mui/material';
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
-
-// @ts-ignore
-export default function UploadPaper({ params }): React.ReactNode {
-	const [files, setFiles] = useState<File[]>([]);
-
-	const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
-		event.preventDefault();
+interface UploadPaperParams {
+	params: {
+		id: string;
 	};
+}
 
-	const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
-		event.preventDefault();
-		const newFiles = Array.from(event.dataTransfer.files);
-		setFiles((prevFiles) => [...prevFiles, ...newFiles]);
-	};
+export default async function UploadPaper({ params }: UploadPaperParams) {
+	const { isAuthenticated, getUser } = getKindeServerSession();
+	const isLogged = await isAuthenticated();
 
-	const handleFileSelect = (event: React.ChangeEvent<HTMLInputElement>) => {
-		if (event.target.files) {
-			const fileArray = Array.from(event.target.files);
-			setFiles((prevFiles) => [...prevFiles, ...fileArray]);
-		}
-	};
+	if (!isLogged) {
+		redirect('/');
+	}
 
-	return (
-		<>
-			<Stack spacing={2}>
-				<Stack>
-					<Typography variant="h5">AI in healthcare</Typography>
-					<Typography variant="subtitle1">Upload your papers</Typography>
-				</Stack>
+	const currentProjectId = Number(params.id); // TODO: add validation
 
-				<Box
-					onDragOver={handleDragOver}
-					onDrop={handleDrop}
-					sx={{
-						p: 2,
-						border: '2px dashed #ccc',
-						minHeight: 100,
-						cursor: 'pointer',
-						display: 'flex',
-						flexDirection: 'column',
-						justifyContent: 'center',
-						alignItems: 'center',
-					}}
-				>
-					<Typography>Drag and drop some files here, or click to select files</Typography>
-					<input
-						type="file"
-						multiple
-						style={{ display: 'none' }}
-						id="file-upload"
-						onChange={handleFileSelect}
-					/>
-					<label htmlFor="file-upload">
-						<Button variant="contained" component="span" startIcon={<CloudUploadIcon />}>
-							Select Files
-						</Button>
-					</label>
-				</Box>
-
-				<List>
-					{files.map((file, index) => (
-						<ListItem key={index}>
-							{file.name} - {file.size} bytes
-						</ListItem>
-					))}
-				</List>
-			</Stack>
-		</>
-	);
+	return <UploadPaperClientComponent currentProjectId={currentProjectId} />;
 }
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,11 +1,20 @@
 import prisma from '../../lib/db';
 import { ProjectsPageClientComponent } from '../../components/projects-page';
 import React from 'react';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { redirect } from 'next/navigation';
 
 const tableColumns: string[] = ['Project Title', 'Created Date', 'Processed Papers', 'Number of Queries', 'Options'];
 const dataKeysOrder: string[] = ['title', 'createdAt', 'papersProcessed', 'queries'];
 
 export default async function Projects() {
+	const { isAuthenticated, getUser } = getKindeServerSession();
+	const isLogged = await isAuthenticated();
+
+	if (!isLogged) {
+		redirect('/');
+	}
+
 	const projects = await prisma.project.findMany();
 
 	return <ProjectsPageClientComponent data={projects} columns={tableColumns} dataKeysOrder={dataKeysOrder} />;
